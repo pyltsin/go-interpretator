701614b866aa863527f98a18c14c0ebba3bc9583
pyltsin-m <pyltsin-m@yandex.ru>
2020-11-14 22:33:04 +0300

    3

--- repl/repl.go
+++ repl/repl.go
@@ -3,7 +3,9 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"go-interpretator/evaluator"
 	"go-interpretator/lexer"
+	"go-interpretator/object"
 	"go-interpretator/parser"
 	"io"
 )
@@ -12,6 +14,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -26,8 +29,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
